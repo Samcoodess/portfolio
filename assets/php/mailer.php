50965d5c6cae9e5a071045468b007fa024530f12
Sambridhi Deo <sambridhdev63@gmail.com>
2023-01-03 00:49:08 -0500

    Projects updates

--- assets/php/mailer.php
+++ assets/php/mailer.php
@@ -1,45 +1,70 @@
 <?php
 
-    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+/**
+ * This example shows how to handle a simple contact form safely.
+ */
 
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+//Import PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
 
-        
-        // if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //     http_response_code(400);
-        //     echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-        //     exit;
-        // }
+$msg = '';
+//Don't run this unless we're handling a form submission
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
 
-        // Change your email hosting in hire
-        $recipient = "sambridhidev63@gmail.com"; 
+    require '../vendor/autoload.php';
 
-        // Set the email subject.sss    
-        $subject = "New contact from $name";
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer();
+    //Send using SMTP to localhost (faster and safer than using mail()) – requires a local mail server
+    //See other examples for how to use a remote server such as gmail
+    $mail->isSMTP();
+    $mail->Host = 'localhost';
+    $mail->Port = 25;
 
-
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
-
-
-        $email_headers = "From: $name <$email>";
-
-
-        mail($recipient, $subject, $email_content, $email_headers)
-            // http_response_code(200);
-            // echo "Thank You for joining my family! I will get back to you soon";
-    //     } else {
-    //         http_response_code(500);
-    //         echo "Oops! Something went wrong and we couldn't send your message.";
-    //     }
-
-    // } else {
-    //     http_response_code(403);
-    //     echo "There was a problem with your submission, please try again.";
-    // }
-
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('from@example.com', 'First Last');
+    //Choose who the message should be sent to
+    //You don't have to use a <select> like in this example, you can simply use a fixed address
+    //the important thing is *not* to trust an email address submitted from the form directly,
+    //as an attacker can substitute their own and try to use your form to send spam
+    $addresses = [
+        // 'sales' => 'sales@example.com',
+        // 'support' => 'support@example.com',
+        'accounts' => 'deosambridhi@gmail.com',
+    ];
+    //Validate address selection before trying to use it
+    if (array_key_exists('dept', $_POST) && array_key_exists($_POST['dept'], $addresses)) {
+        $mail->addAddress($addresses[$_POST['dept']]);
+    } else {
+        //Fall back to a fixed address if dept selection is invalid or missing
+        $mail->addAddress('support@example.com');
+    }
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        //Keep it simple - don't use HTML
+        $mail->isHTML(false);
+        //Build a simple message body
+        $mail->Body = <<<EOT
+Email: {$_POST['email']}
+Name: {$_POST['name']}
+Message: {$_POST['message']}
+EOT;
+        //Send the message, check for errors
+        if (!$mail->send()) {
+            //The reason for failing to send will be in $mail->ErrorInfo
+            //but it's unsafe to display errors directly to users - process the error, log it on your server.
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+            $msg = 'Message sent! Thanks for contacting us.';
+        }
+    } else {
+        $msg = 'Invalid email address, message ignored.';
+    }
+}
 ?>
